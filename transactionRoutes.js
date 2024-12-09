1a76421a6c0b8f3b5f102e1965be9ccc0911ceca
anika-tabassum-10 <anika.tabassum.omi@g.bracu.ac.bd>
2024-12-09 12:41:43 +0600

    Added new files

--- transactionRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { addTransaction, getTransactions } = require('../controllers/transactionController').default;
-const router = express.Router();
-
-router.route('/').post(addTransaction).get(getTransactions);
-
-module.exports = router;
--- userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getUserProfile, updateUserProfile } = require('../controllers/userController').default;
-const router = express.Router();
-
-router.route('/:id').get(getUserProfile).put(updateUserProfile);
-
-module.exports = router;
